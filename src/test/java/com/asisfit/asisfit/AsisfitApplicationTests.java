898b627d43cfe392135ee7141358e9ca8d009451
German Cabrera Aleman <germancabrerita@gmail.com>
2026-07-12 12:22:08 +0200

    feat: setup inicial con SpringBoot, JPA, PostgreSQL y Lombok

---
+++ src/test/java/com/asisfit/asisfit/AsisfitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asisfit.asisfit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsisfitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
